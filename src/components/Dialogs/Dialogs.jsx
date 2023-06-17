f95ab8f7f82c40fcdda4222f722e9b249d2355c0
MihPar <9496260@gmail.com>
2023-06-17 21:38:27 +0300

    eight

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,20 +1,41 @@
 import React from "react";
 import object from "./Dialogs.module.css";
+import {NavLink} from "react-router-dom"
 
+const DialogsItems = (props) => {
+    let path = "/dialogs/" + props.id
+    return (
+        <div>
+            <NavLink to={path} className={navData => navData.isActive ? object.active : object.dialogsItems}>{props.name}</NavLink>
+        </div>
+    )
+}
+
+const Messages = (props) => {
+    let path = "/messages/" + props.id
+    return (
+        <div>
+            <NavLink to={path}  className={navData => navData.isActive ? object.active : object.messages}>{props.message}</NavLink>
+        </div>
+    )
+}
+ 
 const Dialogs = () => {
   return(
-     <div className={object.dialoges}>
-        <div className={object.dialogesItems}>
-            <div className={object.dialog + " " + object.active}>UserOne</div>
-            <div className={object.dialog}>UserTwo</div>
-            <div className={object.dialog}>UserThree</div>
-            <div className={object.dialog}>UserFour</div>
+     <div className={object.dialogs}>
+        <div className={object.dialogsItems}>
+            <DialogsItems name="Mickhael" id="1" />
+            <DialogsItems name="Tatiana" id="2" />
+            <DialogsItems name="Alexandr" id="3" />
+            <DialogsItems name="Maria" id="4" />
+            <DialogsItems name="Ilia" id="5" />
         </div>
         <div className={object.messages}>
-            <div className={object.message}>Hi</div>
-            <div className={object.message}>How are your?</div>
-            <div className={object.message}>Not bad</div>
-            <div className={object.message }>I am fun</div>
+            <Messages message="Hi" id="1"/>
+            <Messages message='How are you' id="2" />
+            <Messages message="Not bad" id="3" />
+            <Messages message="I am fun" id="4" />
+            <Messages message="It is cool" id="5" />
         </div>
      </div>
   )
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,20 +1,24 @@
-import object from "./MyPosts.module.css"
-import Post from "./Posts/Post"
+import object from "./MyPosts.module.css";
+import Post from "./Posts/Post";
 
 const MyPosts = () => {
-    return (
-            <div>
-                My posts
-                <div>
-                    <textarea></textarea>
-                    <button>Add post</button>
-                    <button>Remove</button>
-                </div>
-                <Post message="Hey, how are your?" likesCount="34" />
-                <Post message="It is my first posts" likesCount='56' />
-                <Post countLike="15" />
-            </div>
-    )
-}
+  return (
+    <div className={object.descriptionBlock}>
+      <h3>My posts</h3>
+      <div>
+        <div>
+          <textarea></textarea>
+        </div>
+        <button>Add post</button>
+        <button>Remove</button>
+      </div>
+      <div className={object.mePosts}>
+        <Post message="Hey, how are your?" likesCount="34" />
+        <Post message="It is my first posts" likesCount="56" />
+        <Post countLike="15" />
+      </div>
+    </div>
+  );
+};
 
-export default MyPosts 
+export default MyPosts;
