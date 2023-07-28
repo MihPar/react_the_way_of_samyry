61f4bdd083e6acef9111146fd48ab5216f4b3b57
MihPar <9496260@gmail.com>
2023-07-28 19:43:16 +0300

    forty nine

--- src/components/Profile/ProfileInfo/ProfileStatus.text.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.text.jsx
@@ -8,16 +8,25 @@ describe("ProfileStatus Component", () => {
         const instance = component.getInstanse()
         expect(instance.state.status).toBe("it-incubator.com")
     })
-    test(`after creation, <span> should be displaed`, () => {
+    test("after creation, <span> shouldn`t be displayed", () => {
         const component = create(<ProfileStatus status='it-incubator.com' />)
-        const instance = component.getInstanse()
-        let span = instance.findByType("span")
-        expect(span. length).toBe(1)
+        const root = component.root 
+        expect(() => {
+            let input = root.findByType("span")
+        }).toThrow()
     })
     test(`after creation, <span> with status should contains correct status`, () => {
         const component = create(<ProfileStatus status='it-incubator.com' />)
+        const root = component.root 
+        let span = root.findByType("span")
+        expect(span.children[0]).toBe('it-incubator.com')
+    })
+
+    test(`callback should be called`, () => {
+        const mockCallback = jest.fn()
+        const component = create(<ProfileStatus status='it-incubator.com' updateStatus={mockCallback} />)
         const instance = component.getInstanse()
-        let span = instance.findByType("span")
-        expect(span. length).toBe(1)
+        instance.deactivateEditMode()
+        expect(mockCallback.mock.calls.length).toBe(1 )
     })
 })
