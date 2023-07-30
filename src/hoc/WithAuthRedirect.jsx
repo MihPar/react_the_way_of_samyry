e98d795d2ab686f1077d2145d3d36cc9b253fc5d
MihPar <9496260@gmail.com>
2023-07-30 22:04:57 +0300

    fifty four

--- src/hoc/WithAuthRedirect.jsx
+++ src/hoc/WithAuthRedirect.jsx
@@ -1,10 +1,20 @@
 import React from "react"
 
-let withSuspense = (Component) => {
-    
-    return (props) => {
-        return <React.Suspense fallback={<Preloader />}><Component {...props} /></React.Suspense> 
+let mapStateToPropsForRedirect = (state) => ({
+    isAuth: state.auth.isAuth
+})
+
+export const withAuthRedirect = (Component) => {
+    class RedirectComponent extends React.Component {
+        render() {
+            if(!this.props.isAuth) {
+                return <Redirect to='./login' />
+            } else {
+                return <Component {...this.props} />
+            }
+        }
     }
+    let ConnectedAuthRedirectComponent = connect(mapStateToPropsForRedirect)(RedirectComponent)
 }
 
 export default withSuspense
--- src/hoc/withSuspense.js
+++ src/hoc/withSuspense.js
@@ -2,20 +2,13 @@ import React from "react"
 import { Navigate } from "react-router-dom"
 import { connect } from "react-redux";
 
-let mapStateToPropsForRedirect = (state) => ({
-    isAuth: state.auth.isAuth
-  });
 
-let WithAuthRedirect = (Component) => {
-    class RedirectComponent extends React.Component {
-        render() {
-            if(!this.props.isAuth) return <Navigate to="/login" />
-            return <Component {...this.props} />
-        }
-    }
-    let ConnectedAuthRedirectContainer = connect(mapStateToPropsForRedirect)(RedirectComponent)
-    
-    return ConnectedAuthRedirectContainer
+export const withSuspenses = (Component) => {
+    return (props) => {
+        retrun (
+            <React.Suspense fallback={<Preloader />}><Component {...props} /></React.Suspense> 
+        )
+    }    
 }
 
-export default WithAuthRedirect
+export default withSuspenses
